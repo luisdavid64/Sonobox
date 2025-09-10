37ae96ff88bbfc5a5a787f0510da6008a3c9c9a9
Luis David Reyes Vargas <ge45dej@mytum.de>
2025-09-10 11:27:50 +0200

    Viz not so good

--- DifferentiableMiPhysics/viz_utils.py
+++ DifferentiableMiPhysics/viz_utils.py
@@ -154,7 +154,7 @@ def render_traj_taichi3d(traj, edge_index, masses=None, radii=None, k=None,
                          out_path="traj3d.mp4", fps=30, sim_rate=1000):
     if hasattr(traj, "detach"): traj = traj.detach().cpu().numpy()
     if hasattr(edge_index, "detach"): edge_index = edge_index.detach().cpu().numpy()
-    steps_per_frame = int(44100 / fps)  # 16
+    steps_per_frame = int(16000 / fps)  # 16
     traj = traj[::steps_per_frame]
     T, N, _ = traj.shape
     i, j = edge_index
