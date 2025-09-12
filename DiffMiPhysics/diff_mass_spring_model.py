b2941538ae20cf6ecbf6c45379402e2e042b5b02
Luis David Reyes Vargas <ge45dej@mytum.de>
2025-09-12 15:51:10 +0200

    Fix

--- DiffMiPhysics/diff_mass_spring_model.py
+++ DiffMiPhysics/diff_mass_spring_model.py
@@ -440,7 +440,7 @@ class MassSpringModel(nn.Module):
 
 
 if __name__ == "__main__":
-    fs = 16000
+    fs = 44100
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model = MassSpringModel.from_json(
@@ -479,8 +479,8 @@ if __name__ == "__main__":
     )  # [T_audio, 1]
     
     import soundfile as sf, sounddevice as sd
-    sf.write("mass_spring.wav", audio.detach().cpu().numpy(), 16000)
-    sd.play(audio.detach().cpu().numpy(), 16000); sd.wait()
+    sf.write("mass_spring.wav", audio.detach().cpu().numpy(), fs)
+    sd.play(audio.detach().cpu().numpy(), fs); sd.wait()
 
     # loss = torch.mean(audio**2)
     # print("Audio loss:", loss.item())
--- DiffMiPhysics/diff_mass_spring_model_tied_exp.py
+++ DiffMiPhysics/diff_mass_spring_model_tied_exp.py
@@ -463,8 +463,8 @@ if __name__ == "__main__":
     )  # [T_audio, 1]
     
     import soundfile as sf, sounddevice as sd
-    sf.write("mass_spring.wav", audio.detach().cpu().numpy(), 16000)
-    sd.play(audio.detach().cpu().numpy(), 16000); sd.wait()
+    sf.write("mass_spring.wav", audio.detach().cpu().numpy(), fs)
+    sd.play(audio.detach().cpu().numpy(), fs); sd.wait()
 
     loss = torch.mean(audio**2)
     print("Audio loss:", loss.item())
