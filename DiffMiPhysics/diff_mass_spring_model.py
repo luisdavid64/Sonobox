33df25d0fea5bd1652d8f390485bec61d7dc219d
Luis David Reyes Vargas <ge45dej@mytum.de>
2025-09-12 14:11:07 +0200

    Fix

--- DiffMiPhysics/diff_mass_spring_model.py
+++ DiffMiPhysics/diff_mass_spring_model.py
@@ -5,7 +5,7 @@ from torch import nn
 from typing import Optional
 from topology_utils import build_grid_nodes, build_edges_by_type, dedupe_undirected
 from viz_utils import plot_model_graph_3d, render_traj_taichi3d, plot_spectrogram
-from audio_helpers import axis_pick_t, stereo_mixer_t, mix_down
+from audio_helpers import axis_pick_t, mix_down
 import json
 
 class MassSpringModel(nn.Module):
